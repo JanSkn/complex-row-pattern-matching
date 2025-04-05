7806462cc4acb8ca6c155bfbcadf29a62f962dcf
JanSkn <jan.a.skowron@gmail.com>
2025-04-05 11:38:46 +0200

    changed directory

--- benchmarks/sort_dataset.py
+++ benchmarks/sort_dataset.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv("benchmarks/source/Montreal_Crime_Data.csv", index_col=0)
+df = pd.read_csv("benchmarks/dataset/Montreal_Crime_Data.csv", index_col=0)
 
 df['date'] = pd.to_datetime(df['date'])
 df = df.sort_values(by='date')
@@ -8,4 +8,4 @@ df = df.sort_values(by='date')
 df = df.reset_index(drop=True)
 df.insert(0, 'id', range(len(df)))
 
-df.to_csv("benchmarks/source/Montreal_Crime_Data.csv", index=False)
+df.to_csv("benchmarks/dataset/Montreal_Crime_Data.csv", index=False)
